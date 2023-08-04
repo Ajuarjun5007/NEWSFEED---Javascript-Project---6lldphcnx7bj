58c73c88c208f1092cca001d63530d4e228b8f6f
Ajuarjun5007 <115939598+Ajuarjun5007@users.noreply.github.com>
2023-08-04 10:05:24 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,170 +1,390 @@
-let savedarray=[]
-const allCategoryUrl =
-  "https://content.newtonschool.co/v1/pr/64806cf8b7d605c99eecde47/news";
+// varibles
+//let apiarray=["3b4613ef7b604936bbc82426671ddb95","b8155f2365844483ba6e79d41006c458"]
+// navbar
+const businessbtn = document.getElementById("business");
+const sportsbtn = document.getElementById("sports");
+const entertainmentbtn = document.getElementById("entertainment");
+const technologybtn = document.getElementById("technology");
+const loginbtn = document.querySelector(".fa-solid.fa-user");
+const searchbtn = document.querySelector(".search-btn");
+const closebtnicon = document.querySelector(".close-icon-btn");
+const country = document.getElementById("countrySelect");
+const categorypage = document.querySelector(".categorycards");
+const frontpage = document.querySelector(".newsbar");
+const headlinespage = document.querySelector(".maincards-1");
+const headlinescards = document.querySelector(".subcards");
+const trendingpage = document.querySelector(".maincards-2");
+const newsbar = document.querySelector(".newsbar");
+const trendingcards = document.querySelector(".subcards-2")
+const headlines = "https://newsapi.org/v2/top-headlines?country=in&apiKey=3b4613ef7b604936bbc82426671ddb95";
+const trending = "https://newsapi.org/v2/top-headlines?country=in&category=general&apiKey=3b4613ef7b604936bbc82426671ddb95"
+const categorytitle = document.querySelector(".category-title");
+const categorydisplay = document.querySelector('.categorycardsdsply');
+const bookmarkbtn = document.querySelector(".bookmarks");
+let countryNameKey = "in";
 
-const cardContainer = document.getElementsByClassName("news-bar")[0];
-let alldata=[];
-async function getApiData(url) {
-  try {
-    const response = await fetch(url);
-    const data =  await response.json();
-    // console.log("fetch");
-    // console.log(data);
-    alldata=data;     // this will return object ,but outside it will return promise
-    return data;
-    // createCards(data);
-  } catch (error) {
-    console.log("Error is:" + error);
-  }
+function urlUpdate(nation, category) {
+    return `https://newsapi.org/v2/top-headlines?country=${nation}&category=${category}&apiKey=3b4613ef7b604936bbc82426671ddb95`
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    // document.querySelector('.newsbar').style.display = "flex";
+    headlinesfetch(headlines);
+    trendingfetch(trending);
+})
+
+
+//data fetch
+async function fetchnews(url) {
+    try {
+        const response = await fetch(url)
+        const data = await response.json();
+        newsarr = data.articles;
+        // console.log(newsarr)
+        return newsarr;
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// fetchcategory
+async function categoryfetch(url) {
+    try {
+        const news = await fetchnews(url);
+        news.map((item) => {
+            if (item.description != null) {
+                categorydisplay.innerHTML +=
+                    `
+    <div class="categorycard">
+    <i class="fa-sharp fa-solid fa-bookmark" style="color:dimgray;"></i>
+    <a  href="${item.url}" target="_blank">
+    <div class="categoryimg">
+     <img src="${item.urlToImage}" alt="no-preview">
+     </div>
+     <div class="categorycontent">${item.description}</div>
+     </a>
+     </div>
+     ` } else {
+        categorydisplay.innerHTML +=
+     ` <div class="categorycard2">
+        <i class="fa-sharp fa-solid fa-bookmark" style="color:dimgray;"></i>
+        <div class="categoryimg2">
+        <img src="https://resize.indiatvnews.com/en/resize/newbucket/1200_-/2020/09/breakingnews-live-blog-1568185450-1595123397-1600393902.jpg" alt="no-preview">
+        </div>
+        <div class="categorycontent2">
+        <a  href="${item.url}" target="_blank"> Click Here</a>
+        
+        </div>
+     </div>`
+            }
+        })
+    } catch (error) {
+        //  alert("details not available")    
+    }
+}
+
+categoryfetch(urlUpdate)
+
+country.addEventListener("click", (e) => {
+    countryNameKey = e.target.value;
+    // categoryfetch(searchurl)
+})
+
+businessbtn.addEventListener("click", (e) => {
+    frontpage.style.display = "none";
+    categorydisplay.innerHTML = ""
+    categorypage.style.display = "flex"
+    categorytitle.innerHTML = "<h3>Business</h3>";
+    let category = e.target.innerText.toLowerCase();
+    let url = urlUpdate(countryNameKey, category);
+    categoryfetch(url);
+})
+
+sportsbtn.addEventListener("click", (e) => {
+    frontpage.style.display = "none";
+    categorydisplay.innerHTML = ""
+    categorypage.style.display = "flex"
+    categorytitle.innerHTML = "<h3>Sports</h3>";
+    let category = e.target.innerText.toLowerCase();
+    let url = urlUpdate(countryNameKey, category);
+    categoryfetch(url);
+})
+technologybtn.addEventListener("click", (e) => {
+    frontpage.style.display = "none";
+    categorydisplay.innerHTML = ""
+    categorypage.style.display = "flex"
+    categorytitle.innerHTML = "<h3>Technology</h3>";
+    let category = e.target.innerText.toLowerCase();
+    let url = urlUpdate(countryNameKey, category);
+    categoryfetch(url);
+
+})
+entertainmentbtn.addEventListener("click", (e) => {
+    frontpage.style.display = "none";
+    categorydisplay.innerHTML = ""
+    categorypage.style.display = "flex"
+    categorytitle.innerHTML = "<h3>Entertainment</h3>";
+    let category = e.target.innerText.toLowerCase();
+    let url = urlUpdate(countryNameKey, category);
+    categoryfetch(url);
+})
+loginbtn.addEventListener("click", (e) => {
+    let category = e.target.innerText.toLowerCase();
+    let url = urlUpdate(countryNameKey, category);
+    categoryfetch(url);
+})
+
+searchbtn.addEventListener("click", (e) => {
+    frontpage.style.display = "none";
+    categorydisplay.innerHTML = ""
+    categorypage.style.display = "flex"
+    let searchinput = document.querySelector(".search").value;
+
+    let searchurl = `https://newsapi.org/v2/everything?q=${searchinput}&apiKey=3b4613ef7b604936bbc82426671ddb95`;
+    console.log(searchinput)
+    //  categorytitle.innerHTML="";
+    categoryfetch(searchurl)
+
+
+})
+searchbtn.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+        //   event.preventDefault();
+        frontpage.style.display = "none";
+        categorydisplay.innerHTML = ""
+        categorypage.style.display = "flex"
+        let searchinput = document.querySelector(".search").value;
+
+        let searchurl = `https://newsapi.org/v2/everything?q=${searchinput}&apiKey=b8155f2365844483ba6e79d41006c458`;
+        console.log(searchinput)
+        // categorytitle.innerHTML="";
+        categoryfetch(searchurl)
+    }
+});
+
+
+
+async function headlinesfetch(headlines) {
+    const headlinesnews = await fetchnews(headlines);
+    headlinesnews.map((item, index) => {
+
+        if (index == 0) {
+            headlinespage.innerHTML =
+    `<i class="fa-sharp fa-solid fa-bookmark" style="color:dimgray;"></i>
+         <a  href="${item.url}" target="_blank">
+        <img src="${item.urlToImage}" alt="">
+        <div class="para-1">
+        ${item.description}
+        </div>
+        </a>`
+        }
+
+        if (index != 0) {
+            headlinescards.innerHTML +=
+                `<div class="subcard">
+            <i class="fa-sharp fa-solid fa-bookmark" style="color:dimgray;"></i>
+                <a  href="${item.url}" target="_blank">
+                <img src="${item.urlToImage}" alt=""></img>
+                <div class="para">
+                ${item.description};
+                </div>
+                </a>
+                </div>`
+        }
+    })
+
+}
+
+function sortByPublishedAt(newsData) {
+    return newsData.sort((a, b) => new Date(a.publishedAt) - new Date(b.publishedAt));
+}
+async function trendingfetch(trending) {
+    const news = await fetchnews(trending);
+    const trendingnews = sortByPublishedAt(news);
+    trendingnews.map((item, index) => {
+        console.log(item.publishedAt);
+        if (index == 0) {
+            trendingpage.innerHTML =
+                `<i class="fa-sharp fa-solid fa-bookmark" style="color: dimgray;"></i> 
+        <a  href="${item.url}" target="_blank">
+        <img src="${item.urlToImage}" alt="">
+        <div class="para-1">
+        ${item.description}
+        </div>
+        </a>`
+        }
+
+        if (index != 0) {
+            trendingcards.innerHTML +=
+                `<div class="subcard">   
+            <i class="fa-sharp fa-solid fa-bookmark" style="color:dimgray;"></i>
+             <a  href="${item.url}" target="_blank">
+                <img src="${item.urlToImage}" alt=""></img>
+                <div class="para">
+                ${item.description};
+                </div>
+                </div>
+                </a>`
+        }
+    })
 }
 
 
-function createCards(dataArray, type="main") {
-  // console.log(dataArray);
-  cardContainer.innerHTML="";
-  if(type=="main"){
-    dataArray.map((item) => {
-      cardContainer.innerHTML += 
-      `<div class="news-item">
-          <div class="news-title">
-          <p>by ${item[" author"]}</p>
-          <p>category ${item[" category"]}</p>
-          </div>
-          <div class="news-content">
-                  <p>${item.content}</p>
-                  <a href="${item.url}">Read More</a></div>
-                  <i class="fa-solid fa-heart"></i>
-                  </div>
-                  </div>`;
-
-    });
-  }else{
-    dataArray.map((item) => {
-      cardContainer.innerHTML += 
-      `<div class="news-item">
-          <div class="news-title">
-          <p>by ${item[" author"]}</p>
-          <p>category ${item[" category"]}</p>
-          </div>
-          <div class="news-content">
-                  <p>${item.content}</p>
-                  <a href="${item.url}">Read More</a></div>
-                  <i id="${item[" category"]}" onClick="removeFromSavedList(event)" class="fa-solid fa-circle-xmark"></i>
-                  </div>
-                  </div>`;
-
-    });
-  }
+newsbar.onclick = (e) =>{
+    if(e.target.className==="fa-sharp fa-solid fa-bookmark"){
+            console.log(e.target.className);
+            console.log(e.target.style.color);
+           if(e.target.style.color==="dimgray"){
+             e.target.style.color="yellow";
+             addToLocalStorage(e);
+            }else if(e.target.style.color==="yellow"){
+                e.target.style.color="dimgray";
+                removeFromSavedList(e);
+            
+            }
+}
+}
+categorydisplay.onclick = (e) =>{
+    if(e.target.className==="fa-sharp fa-solid fa-bookmark"){
+            console.log(e.target.className);
+            console.log(e.target.style.color);
+           if(e.target.style.color==="dimgray"){
+             e.target.style.color="yellow";
+             addToLocalStorage(e);
+            }else if(e.target.style.color="yellow"){
+                e.target.style.color==="dimgray";
+                removeFromSavedList(e);
+            }
+}
+}
+bookmarkbtn.onclick = () => {
+showbookmark();
 }
 
-async function fetchAndGenerate(url, myFunction) {
-  const data = await getApiData(url);
-  myFunction(data);
-}
+function showbookmark (){
+    let bookmarkarr = JSON.parse(localStorage.getItem("savedNews"));
+    frontpage.style.display = "none";
+    categorydisplay.innerHTML = ""
+    categorypage.style.display = "flex"
+    categorytitle.innerHTML = "<h1>Bookmarks</h1>";
+    bookmarkarr.map((item)=>{
+     categorydisplay.innerHTML +=
+        `<div class="categorycard">
+<i class="fa-sharp fa-solid fa-bookmark" style="color:yellow;"></i>
+<a  href="${item.url}" target="_blank">
+<div class="categoryimg">
+<img src="${item.urlToImage}" alt="no-preview">
+</div>
+<div class="categorycontent">${item.description}</div>
+</a>
+</div>
+` 
+    })
+} 
 
-fetchAndGenerate(allCategoryUrl, createCards);
-// ------------
+let savedarray = []
 
-const categoryselect=document.querySelector(".category-bar");
-// console.log(categoryselect)
+function addToLocalStorage(e) {
+    let description = e.target.parentNode.innerText;
+    let url = e.target.parentNode.children[1].href;
+    let img = e.target.parentNode.children[1].childNodes[1].currentSrc;
 
-let result=[];
-categoryselect.onclick = (e) =>{
-  let val=e.target.innerHTML;
-  if(val==="All"){
-    cardContainer.innerHTML = ''
-    fetchAndGenerate(allCategoryUrl, createCards);
-  }else{
-    result= alldata.filter((item)=>(item[" category"]===val))
- cardContainer.innerHTML = ''
-//  return filtereddata
-createCards(result)
-  }
- 
-// console.log(result)
-}
-
-
-cardContainer.onclick=(e)=>{
-  console.log(e)
-  if(e.target.className==="fa-solid fa-heart"){
-    //  e.target.classList.add="fa-solid fa-heart"
-      
-      if(e.target.style.color==="red"){
-        e.target.style.color=""
-        removeFromSavedList(e);
-      }else{
-        e.target.style.color="red"
-        addToLocalStorage(e);
-      }
-  }
-
-  // create array heart-> that particular card ->
-}
-
-// collapsable side bar//
-const opensidebarbtn = document.getElementById("sidebar");
-opensidebarbtn.onclick=(e)=>{
-  document.getElementById("mySidebar").style.width = "250px";
-  // document.getElementById("mySidebar").style.height = "1000px";
-  document.getElementById("main").style.marginLeft = "250px";
-}
-
-const closesidebarbtn = document.querySelector(".closebtn");
-closesidebarbtn.onclick=(e)=>{
-  document.getElementById("mySidebar").style.width = "0";
-  // document.getElementById("mySidebar").style.height = "0";
-  document.getElementById("main").style.marginLeft="0";
-}
-
-const loginaccess = document.getElementById("loginaccess");
-loginaccess.onclick=(e) =>{
-  
-}
-// saved items
-/* // saved items
-1. heart button click - add that card to empty array
-2. then run a function that adds that array to local storage
-3. on click of saved items button - run a function that fetches local storage data and pass that data to create cards.
-*/
-
-function addToLocalStorage (e){
-  let author=e.target.parentElement.firstElementChild.firstElementChild.innerHTML ;
-  let category=e.target.parentElement.firstElementChild.lastElementChild.innerHTML;
-  let content=e.target.previousElementSibling.firstElementChild.innerText;
-  let url=e.target.previousElementSibling.lastElementChild.href;
-  // console.log(array)
-  let currentObject={
-    " category": category,
-    " author":author,
-    "content":content,
-    "url":url
-  }
-  savedarray.push(currentObject);
- localStorage.setItem("savedNews",JSON.stringify(savedarray));
-
-  console.log(savedarray)
-// savedarray.forEach((item)=>{
-// })
- }
-function showFavourites() {
- let favourite = JSON.parse(localStorage.getItem("savedNews"));
-//  console.log(favourite)
- createCards(favourite,"savedList");
-
-
+     console.log(e)
+    let currentObject = {
+        "description": description,
+        "url": url,
+        "urlToImage": img,
+    }
+    savedarray.push(currentObject);
+    localStorage.setItem("savedNews", JSON.stringify(savedarray));
+    console.log(savedarray)
 }
 
 function removeFromSavedList(e){
-  e.stopPropagation();
-  let id = e.target.id;
-  // console.log(id, "--id");
-  newArray = savedarray.filter((item)=>{
-    // console.log(item[" category"] === id, "--item cat");
-    return item[" category"] !== id;
-  });
-  savedarray = [...newArray];
-  // console.log(newArray, "--new array");
-  localStorage.setItem("savedNews",JSON.stringify(savedarray));
-  showFavourites();
-};
+    e.stopPropagation();
+    let description = e.target.parentNode.innerText;
+    console.log(description);
+    newArray = savedarray.filter((item)=>{
+      return item["description"] !== description;
+    });
+    savedarray = [...newArray];
+    // console.log(newArray, "--new array");
+    localStorage.setItem("savedNews",JSON.stringify(savedarray));
+    showbookmark();
+  };
+
+// login functionality
+// javascript code goes here
+
+let signupForm = document.getElementById("signup-form");
+let signinForm  = document.getElementById("signin-form")
+let togglebtn=document.getElementById("toggle-btn");
+
+signupForm.style.display="block";
+signinForm.style.display="none";
+
+togglebtn.addEventListener("click",function() {
+    if(signupForm.style.display==="block"){
+        signupForm.style.display="none"
+        signinForm.style.display="block"
+        togglebtn.textContent="Don't have an account? Sign Up!";
+    }else{
+        signinForm.style.display="none"
+        signupForm.style.display="block"
+        togglebtn.textContent="Already have an account? Sign In!";
+    }
+})
+let loginform = document.querySelector(".loginform");
+loginbtn.onclick=()=>{
+    loginform.style.display="block";
+}
+closebtnicon.onclick = () => {
+    loginform.style.display="none";
+    console.log(closebtnicon);
+}
+
+
+// time fnctionality
+const timeElement = document.querySelector(".date-time");
+// const dateElement=document.querySelector(".date");
+function formatTime(date) {
+    const hours12 = date.getHours();
+    const minutes = date.getMinutes();
+    return `${hours12.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")} IST`
+}
+
+function formatDate(date) {
+    const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    return `${days[date.getDay()]}, ${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`
+}
+setInterval(() => {
+    const now = new Date();
+    timeElement.textContent = formatTime(now) + "  |  ";
+    timeElement.textContent += formatDate(now);
+    // dateElement.textContent = formatDate(now);
+}, 200)
+
+
+
+
+// setTimeout(() => {
+//     document.querySelectorAll('.fa-sharp')[1].addEventListener('click', () => {
+//         console.log("hi");
+//     })
+// }, 1000)
+
+
+
+
+// if(e.target.className==="fa-sharp fa-solid fa-bookmark"){
+//     console.log(e.target.className);
+//     console.log(e.target.style.color);
+//   if(e.target.style.color==="dimgray"){
+//     console.log(e.target.style.color)
+//     e.target.style.color="black";
+//     addToLocalStorage(e);
+//     }
+// 
